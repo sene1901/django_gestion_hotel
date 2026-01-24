bcbd8b21b45787767bfbdabb0df918cc54640389
sene32304@gmail.com <sene32304@gmail.com>
2026-01-24 12:32:55 +0000

    update auj

--- backend_django/urls.py
+++ backend_django/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,6 +30,9 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
+
+path("accounts/token/refresh/", TokenRefreshView.as_view()),
+
 
 ]
 
--- hotels/urls.py
+++ hotels/urls.py
@@ -4,7 +4,7 @@ from .views import hotel_list, hotel_create, hotel_update, hotel_delete
 
 urlpatterns = [
     
-    path('/', hotel_list),
+    path('', hotel_list),
     path('create/', hotel_create),
     path('<int:id>/update/', hotel_update),
     path('<int:id>/delete/', hotel_delete),
