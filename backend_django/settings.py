d487e6e675277e839351969517b1b66056f6cb48
sene32304@gmail.com <sene32304@gmail.com>
2026-01-25 18:11:03 +0000

    update url

--- backend_django/settings.py
+++ backend_django/settings.py
@@ -304,7 +304,7 @@ TEMPLATES = [
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://django-gestion-hotel.onrender.com",
-    "https://projet-stage-gestion-hotel.vercel.app",
+    "https://projet-stage-gestion-hotel.vercel.app/login",
 ]
 
 DEBUG = False
