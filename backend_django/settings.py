33e6e5818c7bc96dc232afac978133d1e99c97bc
sene32304@gmail.com <sene32304@gmail.com>
2026-01-26 13:22:36 +0000

    UPdate seria views3

--- backend_django/settings.py
+++ backend_django/settings.py
@@ -225,11 +225,7 @@ ALLOWED_HOSTS = [
     "localhost",
 ]
 
-# -----------------------------
-# Media
-# -----------------------------
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # -----------------------------
 # Applications
@@ -252,8 +248,6 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'hotels.apps.HotelsConfig',
     'rest_framework_simplejwt.token_blacklist',
-     'cloudinary',
-    'cloudinary_storage', 
 ]
 
 # -----------------------------
@@ -311,16 +305,6 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 
-# Stockage par défaut pour les fichiers uploadés
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-# Paramètres Cloudinary
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'TON_CLOUD_NAME',
-    'API_KEY': 'TON_API_KEY',
-    'API_SECRET': 'TON_API_SECRET',
-}
-
 
 
 WSGI_APPLICATION = 'backend_django.wsgi.application'
@@ -374,8 +358,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # -----------------------------
 # Media files
 # -----------------------------
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # -----------------------------
 # CORS
