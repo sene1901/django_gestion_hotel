0619b44bb02332863cc298346cab13f5f0db76c4
sene32304@gmail.com <sene32304@gmail.com>
2026-01-27 14:01:30 +0000

    fix HotelSerializer import1

--- hotels/serializers.py
+++ hotels/serializers.py
@@ -1,5 +1,7 @@
+
 from rest_framework import serializers
 from .models import Hotel
+
 
 class HotelSerializer(serializers.ModelSerializer):
     image_url = serializers.SerializerMethodField()
@@ -8,22 +10,28 @@ class HotelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hotel
         fields = [
-            'id',
-            'name',
-            'description',
-            'email',        # ← Ajoute
-            'telephone',    # ← Ajoute
-            'prix',
-            'devise',       # ← Ajoute
-            'image',
-            'image_url',
-            'owner',
-            'created_at',
-            'updated_at',
+            "id",
+            "name",
+            "description",
+            "email",
+            "telephone",
+            "prix",
+            "devise",
+            "image",
+            "image_url",
+            "owner",
+            "created_at",
+            "updated_at",
         ]
-        read_only_fields = ['owner', 'created_at', 'updated_at']
+        read_only_fields = ["owner", "created_at", "updated_at"]
 
     def get_image_url(self, obj):
-        if obj.image:
-            return obj.image.url
+        """
+        Sécurisé pour Cloudinary (évite les 500 en prod)
+        """
+        try:
+            if obj.image and hasattr(obj.image, "url"):
+                return obj.image.url
+        except Exception:
+            return None
         return None
--- hotels/urls.py
+++ hotels/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import hotel_list, hotel_create, hotel_update, hotel_delete
+from .views import hotel_list,  HotelCreateView, hotel_update, hotel_delete
 
 
 urlpatterns = [
     
     path('', hotel_list),
-    path('create/', hotel_create),
+    path("create/", HotelCreateView.as_view(), name="hotel-create"),
     path('<int:id>/update/', hotel_update),
     path('<int:id>/delete/', hotel_delete),
 ]
