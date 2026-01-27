7c608ba41cef148f2ad6d9d1a2e2107f1250fab9
sene32304@gmail.com <sene32304@gmail.com>
2026-01-27 14:57:51 +0000

    fix HotelSerializer import2

--- hotels/urls.py
+++ hotels/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import hotel_list,  HotelCreateView, hotel_update, hotel_delete
-
+from .views import (
+    hotel_list,
+    HotelCreateView,
+    hotel_update,
+    hotel_delete,
+)
 
 urlpatterns = [
-    
-    path('', hotel_list),
+    path("", hotel_list, name="hotel-list"),
     path("create/", HotelCreateView.as_view(), name="hotel-create"),
-    path('<int:id>/update/', hotel_update),
-    path('<int:id>/delete/', hotel_delete),
+    path("<int:id>/update/", hotel_update, name="hotel-update"),
+    path("<int:id>/delete/", hotel_delete, name="hotel-delete"),
 ]
